b5ee9a879164e3649535660a116dbcdc9a432283
Alister FAUDEMER <al.faudemer@protonmail.com>
2025-08-26 09:57:46 +0200

    updated models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :events, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :event_registrations, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
