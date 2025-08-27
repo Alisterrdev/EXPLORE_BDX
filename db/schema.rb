171e58398d4f795fef0f5738aa2b5acb7d046225
Alister FAUDEMER <al.faudemer@protonmail.com>
2025-08-27 16:26:33 +0200

    show finito

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-fix_longitude_latitude_migration
-ActiveRecord::Schema[7.1].define(version: 2025_08_27_091535) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_08_27_105956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_27_091535) do
     t.datetime "updated_at", null: false
     t.float "longitude"
     t.float "latitude"
+    t.text "details"
   end
 
   create_table "events_registrations", force: :cascade do |t|
