4fcc2de25d648cbf4a648a8aedc107c36fa80b5b
alexiapierens <alexia.pierens@kedgebs.com>
2025-08-28 15:53:23 +0200

    front2 +js

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ApplicationRecord
   has_many :users, through: :events_registrations, dependent: :destroy
 
   after_validation :geocode, if: :will_save_change_to_address?
-
-  TAGS = ["Festif", "Creatif", "Foodies", "Culturel", "Chill", "Nature", "Gratuit", "Entre 0 et 40 euros", "Plus de 40 euros"]
-
+  TAGS = ["Festif", "Creatif", "Foodies", "Culturel", "Chill", "Nature", "Gratuit", "Entre_0_et_40_euros", "Plus_de_40_euros"]
+  TAGS_MOOD = ["Festif", "Creatif", "Foodies", "Culturel", "Chill", "Nature", ]
+  TAGS_BUDGET = ["Gratuit", "Entre_0_et_40_euros", "Plus_de_40_euros"]
 end
