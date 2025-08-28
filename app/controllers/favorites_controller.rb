8398c62ccfd62d84a17a7dba129257bf5decd8b0
Alister FAUDEMER <al.faudemer@protonmail.com>
2025-08-28 14:53:07 +0200

    cloudinary et les favoris

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,20 @@
 class FavoritesController < ApplicationController
+
+  def index
+    @favorites = Favorite.all
+  end
+
+  # def show
+  #   @favorite = Event.find(params[:id])
+  # end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @favorite = current_user.favorites.new(event: @event)
+    if @favorite.save
+      redirect_to event_path(@event), notice: "Evenement ajouté"
+    else
+      redirect_to event_path(@event), alert: "Cet événement n'est pas accessible"
+    end
+  end
 end
