2db5c176a773c8dc78363ffca46378e8cdd19438
alexiapierens <alexia.pierens@kedgebs.com>
2025-09-01 18:03:43 +0200

    front notif

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :devise_controller?
+
+  layout :layout_by_resource
+
+  private
+  def layout_by_resource
+    devise_controller? ? "devise" : "application"
+  end
 end
